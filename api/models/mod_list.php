6c0342e3a3b948486b6a9c94619e3c840db77c23
Lil-Trill <danil.yurin.1990@mail.ru>
2023-04-26 15:08:51 +0500

    sort

--- api/models/mod_list.php
+++ api/models/mod_list.php
@@ -3,6 +3,10 @@
     <input type="range" id="age" name="age" min="0" value="110" max="110" step="1" oninput="level.value = age.valueAsNumber">
     <output for="age" name="level">110</output>
     <input type="submit" value="Фильтровать">
+    <input type="radio" name="sort-age" value="asc" id="sort-asc">
+    <label for="sort-asc">по-возрастанию</label>
+    <input type="radio" name="sort-age" value="desc" id="sort-desc">
+    <label for="sort-desc">по-убыванию</label>
 </form>
 
 <?php
@@ -13,8 +17,15 @@ if(isset($_POST['age'])){
     $age = $this->formatstr($_POST['age']);
     $sql .= "WHERE age < $age";
 }
+
+if(isset($_POST['sort-age'])){
+    $sort = $this->formatstr($_POST['sort-age']);
+    $sql .= " ORDER BY age $sort";
+}
+
 //выполнить запрос
 $result = $this->connect->query("SELECT * FROM students".$sql);
+print_r("SELECT * FROM students".$sql);
 //вывести результат
 //цикл выполняется пока в row записываются картежи из БД
 while($row = $result->fetch_assoc()){
